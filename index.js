4729a24193ae44e456336f8a756796514e874f1b
Cartrell KIDD <cartrellk56@gmail.com>
2021-09-26 19:51:20 -0400

    Localstorage set

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     var textAreas = $('textarea')
     var hour = moment().hour();
-    
-    for (var i = 0; i < textAreas.length; i++)  {
+    getItems()
+
+    for (var i = 0; i < textAreas.length; i++) {
         var currentTextArea = textAreas[i]
         var currentId = parseInt(currentTextArea.id)
         // If hour is less than Hour represented
-        if(hour < currentId) {
+        if (hour < currentId) {
             $(currentTextArea).addClass("future");
         }
         else if (hour > currentId) {
@@ -22,15 +23,34 @@ $(document).ready(function() {
             $(currentTextArea).addClass("present");
         }
     }
+
+    function notify(event) {
+
+        var id = event.target.id;
+        var text = document.getElementById(id + "-textArea").value;
+
+        localStorage.setItem(id, text);
+
+    }
+
+    function getItems() {
+        var id = "10am";
+        document.getElementById(id + "-textArea").value = localStorage.getItem(id)
+
+
+    }
+    $(".saveBtn").on("click", notify);
+
+
 })
-           
-
-            
-
-            
 
 
 
-        
-  
-    
+
+
+
+
+
+
+
+
